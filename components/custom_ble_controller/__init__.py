361510960ae810d24611a0968bf1c94d0521e4e6
caglar <caglarglck@yandex.com>
2024-08-27 05:33:29 +0300

    test

--- components/custom_ble_controller/__init__.py
+++ components/custom_ble_controller/__init__.py
@@ -1,42 +1,65 @@
-from esphome import automation
 import esphome.codegen as cg
-from esphome.components import binary_sensor, esp32_ble_server, output, gpio
+from esphome.components import binary_sensor, esp32_ble_server, output
 import esphome.config_validation as cv
-from esphome import pins
-from esphome.const import CONF_PIN
-
+from esphome.const import CONF_ID
 
 AUTO_LOAD = ["esp32_ble_server"]
+CODEOWNERS = ["@jesserockz"]
+DEPENDENCIES = ["wifi", "esp32"]
 
+CONF_AUTHORIZED_DURATION = "authorized_duration"
+CONF_AUTHORIZER = "authorizer"
 CONF_BLE_SERVER_ID = "ble_server_id"
+CONF_IDENTIFY_DURATION = "identify_duration"
+CONF_STATUS_INDICATOR = "status_indicator"
+CONF_WIFI_TIMEOUT = "wifi_timeout"
 
-simple_led_ns = cg.esphome_ns.namespace('custom_ble_controller')
-SimpleLEDComponent = simple_led_ns.class_('CustomBLEController', cg.Component, esp32_ble_server.BLEServiceComponent)
+esp32_improv_ns = cg.esphome_ns.namespace("esp32_improv")
+ESP32ImprovComponent = esp32_improv_ns.class_(
+    "ESP32ImprovComponent", cg.Component, esp32_ble_server.BLEServiceComponent
+)
 
 
-BLEEnableAction = simple_led_ns.class_("BLEControllerSEND", automation.Action)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(ESP32ImprovComponent),
+        cv.GenerateID(CONF_BLE_SERVER_ID): cv.use_id(esp32_ble_server.BLEServer),
+        cv.Required(CONF_AUTHORIZER): cv.Any(
+            cv.none, cv.use_id(binary_sensor.BinarySensor)
+        ),
+        cv.Optional(CONF_STATUS_INDICATOR): cv.use_id(output.BinaryOutput),
+        cv.Optional(
+            CONF_IDENTIFY_DURATION, default="10s"
+        ): cv.positive_time_period_milliseconds,
+        cv.Optional(
+            CONF_AUTHORIZED_DURATION, default="1min"
+        ): cv.positive_time_period_milliseconds,
+        cv.Optional(
+            CONF_WIFI_TIMEOUT, default="1min"
+        ): cv.positive_time_period_milliseconds,
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SimpleLEDComponent),
-    cv.GenerateID(CONF_BLE_SERVER_ID): cv.use_id(esp32_ble_server.BLEServer),
-    cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
-}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()],)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    pin = await cg.gpio_pin_expression(config[CONF_PIN])
-    cg.add(var.set_pin(pin))
 
     ble_server = await cg.get_variable(config[CONF_BLE_SERVER_ID])
     cg.add(ble_server.register_service_component(var))
 
-def turn_on_action(var):
-    return cg.call(var.turn_on())
+    cg.add_define("USE_IMPROV")
+    cg.add_library("improv/Improv", "1.2.4")
 
-@automation.register_action("custom_ble_controller.enable", BLEEnableAction, cv.Schema({}))
-async def ble_enable_to_code(config, action_id, template_arg, args):
-    return cg.new_Pvariable(action_id, template_arg)
+    cg.add(var.set_identify_duration(config[CONF_IDENTIFY_DURATION]))
+    cg.add(var.set_authorized_duration(config[CONF_AUTHORIZED_DURATION]))
 
+    cg.add(var.set_wifi_timeout(config[CONF_WIFI_TIMEOUT]))
 
+    if CONF_AUTHORIZER in config and config[CONF_AUTHORIZER] is not None:
+        activator = await cg.get_variable(config[CONF_AUTHORIZER])
+        cg.add(var.set_authorizer(activator))
+
+    if CONF_STATUS_INDICATOR in config:
+        status_indicator = await cg.get_variable(config[CONF_STATUS_INDICATOR])
+        cg.add(var.set_status_indicator(status_indicator))
--- components/custom_ble_controller/custom_ble_controller.h
+++ components/custom_ble_controller/custom_ble_controller.h
@@ -1,31 +1,72 @@
 #pragma once
 
-#include "esphome/core/automation.h"
 #include "esphome/core/component.h"
-#include "esphome/core/gpio.h"
 #include "esphome/core/defines.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/preferences.h"
 
-
 #include "esphome/components/esp32_ble_server/ble_characteristic.h"
 #include "esphome/components/esp32_ble_server/ble_server.h"
+#include "esphome/components/wifi/wifi_component.h"
+
+#ifdef USE_BINARY_SENSOR
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#endif
+
+#ifdef USE_OUTPUT
+#include "esphome/components/output/binary_output.h"
+#endif
+
+#include <vector>
+
+#ifdef USE_ESP32
+
+#include <improv.h>
 
 namespace esphome {
-namespace custom_ble_controller {
+namespace esp32_improv {
 
-class CustomBLEController : public Component,  public BLEServiceComponent {
+using namespace esp32_ble_server;
+
+class ESP32ImprovComponent : public Component, public BLEServiceComponent {
  public:
-  void setup() override;
+  ESP32ImprovComponent();
+  void dump_config() override;
   void loop() override;
+  void setup() override;
+  void setup_characteristics();
+  void on_client_disconnect() override;
 
-  void set_pin(GPIOPin *pin) { this->pin_ = pin; }
-  void turn_on();
-  void turn_off();
+  float get_setup_priority() const override;
+  void start() override;
+  void stop() override;
+  bool is_active() const { return this->state_ != improv::STATE_STOPPED; }
+
+#ifdef USE_BINARY_SENSOR
+  void set_authorizer(binary_sensor::BinarySensor *authorizer) { this->authorizer_ = authorizer; }
+#endif
+#ifdef USE_OUTPUT
+  void set_status_indicator(output::BinaryOutput *status_indicator) { this->status_indicator_ = status_indicator; }
+#endif
+  void set_identify_duration(uint32_t identify_duration) { this->identify_duration_ = identify_duration; }
+  void set_authorized_duration(uint32_t authorized_duration) { this->authorized_duration_ = authorized_duration; }
+
+  void set_wifi_timeout(uint32_t wifi_timeout) { this->wifi_timeout_ = wifi_timeout; }
+  uint32_t get_wifi_timeout() const { return this->wifi_timeout_; }
 
  protected:
-  GPIOPin *pin_;
-  bool led_state_{false};
+  bool should_start_{false};
+  bool setup_complete_{false};
+
+  uint32_t identify_start_{0};
+  uint32_t identify_duration_;
+  uint32_t authorized_start_{0};
+  uint32_t authorized_duration_;
+
+  uint32_t wifi_timeout_{};
+
+  std::vector<uint8_t> incoming_data_;
+  wifi::WiFiAP connecting_sta_;
 
   BLEService *service_ = nullptr;
   BLECharacteristic *status_;
@@ -34,15 +75,31 @@ class CustomBLEController : public Component,  public BLEServiceComponent {
   BLECharacteristic *rpc_response_;
   BLECharacteristic *capabilities_;
 
+#ifdef USE_BINARY_SENSOR
+  binary_sensor::BinarySensor *authorizer_{nullptr};
+#endif
+#ifdef USE_OUTPUT
+  output::BinaryOutput *status_indicator_{nullptr};
+#endif
+
+  improv::State state_{improv::STATE_STOPPED};
+  improv::Error error_state_{improv::ERROR_NONE};
+
+  bool status_indicator_state_{false};
+  void set_status_indicator_state_(bool state);
+
+  void set_state_(improv::State state);
+  void set_error_(improv::Error error);
+  void send_response_(std::vector<uint8_t> &response);
+  void process_incoming_data_();
+  void on_wifi_connect_timeout_();
+  bool check_identify_();
 };
 
-extern CustomBLEController *global_bleController;
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+extern ESP32ImprovComponent *global_improv_component;
 
-template<typename... Ts> class BLEControllerSEND : public Action<Ts...> {
- public:
-  void play(Ts... x) override { global_bleController->turn_on(); }
-};
-
-
-}  // namespace simple_led
+}  // namespace esp32_improv
 }  // namespace esphome
+
+#endif
